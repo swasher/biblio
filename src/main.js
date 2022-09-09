b23595e22f8504abc4e96ca4903f499a4aaa7db2
Swasher <mr.swasher@gmail.com>
2022-09-09 13:54:01 +0300

    working...

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+import { firebaseApp } from './firebase'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App).use(router)
+
+app.use(ElementPlus)
+app.use(firebaseApp)
+
+app.mount('#app')
